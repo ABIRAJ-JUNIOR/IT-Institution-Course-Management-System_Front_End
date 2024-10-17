a5fb82ac06b9af27455a82de5e8b883ffb2d0a0f
L-Navee <naveethannaveethan13@gmail.com>
2024-10-17 13:47:22 +0530

    Update student_course_selection.js
    Get All Courses Fetch API has been Updated.

--- 03-Student/02_Student_Course_Selection/student_course_selection.js
+++ 03-Student/02_Student_Course_Selection/student_course_selection.js
@@ -13,6 +13,20 @@ async function GetAllStudents(){
 };
 GetAllStudents()
 
+let courses = [];
+const GetAllCoursesURL = 'http://localhost:5251/api/Course/Get-All-Courses';
+//Fetch Students Data from Database
+async function GetAllCourses(){
+    fetch(GetAllCoursesURL).then((response) => {
+        return response.json();
+    }).then((data) => {
+        courses = data;
+        DuplicateCourseRemove();
+
+    })
+};
+GetAllCourses()
+
 // Remove Duplicates courses from Local Storage
 let uniqueCourses = [];
 async function DuplicateCourseRemove(){
